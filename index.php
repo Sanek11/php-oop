0cb02e1aa0b7e8bd256c1f7534f2c2197b48478b
sanek11 <shura.karetnikov.89@mail.ru>
2024-01-27 13:51:05 +0300

    обращение к методам через имена в ассоциативном массиве

--- index.php
+++ index.php
@@ -4,13 +4,18 @@ require_once 'Employee.php';
 require_once 'Student.php';
 require_once 'Array.php';
 
-// $jey = new Student('Jey');
-// echo $jey->getName() . ' ';
-// echo $jey->transferToNextCourse();
-// echo $jey-> getCourse();
+$jey = new Student('Jey');
+echo $jey->getName() . ' ';
+echo $jey->transferToNextCourse();
+echo $jey-> getCourse();
 
 //work with Array class
 $arr = new Arr;
 $arr->add([1, 2, 3]);
 echo $arr->getAvg();
+
+
+//обращение к методам через имена в ассоциативном массиве
+// $methods = ['method1' => 'getProps'];
+// echo $city->{$methods['method1']}();
 ?>
